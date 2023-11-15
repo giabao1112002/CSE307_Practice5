f9a8e0d3c287e54d3df08a4f1c96a192e7fca05c
giabao <phamgiabao1112002@gmail.com>
2023-11-15 16:07:02 +0700

    adf

---
+++ javascrip.js
@@ -0,0 +1,7 @@
+
+    const Username = document.getElementById(Username).value;
+     const Password = document.getElementById(Password).value;
+if(!Username||!Password){
+    alert('PLEASE FILL ALL!!!');
+    return;
+}
